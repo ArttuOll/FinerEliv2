d2edff82eb6796c6c104f22bb4f895fcdcf474fb
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-08-12 15:01:36 +0300

    Fix Tailwind config

--- FinerEli/tailwind.config.js
+++ FinerEli/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Pages/**/.cshtml"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./Pages/**/*.cshtml"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
