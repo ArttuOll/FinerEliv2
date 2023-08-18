b9a10e911d68b5b91543c399a0bf3961a63ba093
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-08-18 16:53:33 +0300

    Setup DB on launch

--- FinerEli/Program.cs
+++ FinerEli/Program.cs
@@ -1,7 +1,15 @@
+using FinerEli.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<FoodsContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Filename=../../../FinerEli.db")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -12,6 +20,19 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<FoodsContext>();
+    context.Database.EnsureCreated();
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
