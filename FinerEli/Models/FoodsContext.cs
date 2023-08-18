5db56503aaca263ef4681c4e2c4dec29e3e8c8a4
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-08-18 16:53:17 +0300

    Create FoodsContext

---
+++ FinerEli/Models/FoodsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinerEli.Models;
+
+public class FoodsContext : DbContext
+{
+    public FoodsContext(DbContextOptions<FoodsContext> options)
+        : base(options)
+    {
+    }
+
+    public string DbPath { get; }
+
+    public DbSet<Food> Foods { get; set; }
+    public DbSet<ComponentValue> ComponentValues { get; set; }
+    public DbSet<EufdName> EufdNames { get; set; }
+    public DbSet<ComponentUnit> ComponentUnits { get; set; }
+}
