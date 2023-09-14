f9d880ef88ee13bfde9ed35f215eb94e29fd6a05
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-09-14 15:09:05 +0300

    Show food name and its component categories

--- FinerEli/Pages/FoodDetails.cshtml.cs
+++ FinerEli/Pages/FoodDetails.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using FinerEli.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FinerEli.Pages;
@@ -13,10 +14,30 @@ public class FoodDetails : PageModel
         _context = context;
     }
 
-    public Models.Food Food { get; set; } = new Models.Food();
+    public Food Food { get; set; } = new();
     
-    public void OnGet()
+    public IEnumerable<ComponentClass> ComponentClasses { get; set; } = new List<ComponentClass>();
+    
+    public IActionResult OnGet(int id)
     {
+        var food = _context.Foods.Find(id);
+        if (food == null)
+        {
+            return NotFound();
+        }
         
+        Food = food;
+
+        var componentValues = _context.ComponentValues.Where(componentValue => componentValue.FoodId == Food.Id).ToList();
+        
+        var componentClasses = from componentValue in componentValues
+            join component in _context.Components on componentValue.EufdNameThsCode equals component.EufdNameThsCode
+            join componentClass in _context.ComponentClasses on component.ComponentClassName equals componentClass.Name
+            orderby componentValue.Value descending 
+            select componentClass;
+        
+        ComponentClasses = componentClasses.Distinct();
+
+        return Page();
     }
 }
