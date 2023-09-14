863ecc835757246c34477a728f4822468e53f2d4
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-09-14 10:52:07 +0300

    Rename Food page to FoodDetails

---
+++ FinerEli/Pages/FoodDetails.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using FinerEli.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinerEli.Pages;
+
+public class FoodDetails : PageModel
+{
+    
+    private readonly FoodsContext _context;
+
+    public FoodDetails(FoodsContext context)
+    {
+        _context = context;
+    }
+
+    public Models.Food Food { get; set; } = new Models.Food();
+    
+    public void OnGet()
+    {
+        
+    }
+}
