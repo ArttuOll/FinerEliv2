566e077a62084cb3bb77ad3633a61da633a90898
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-09-14 08:54:31 +0300

    Remove boilerplate

--- FinerEli/Pages/Index.cshtml.cs
+++ FinerEli/Pages/Index.cshtml.cs
@@ -5,13 +5,6 @@ namespace FinerEli.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public RedirectToPageResult OnGet()
     {
         return RedirectToPage("/Search");
