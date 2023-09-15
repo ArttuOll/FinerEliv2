90b5cad4bfd5ee026632e49d6119a572eada7576
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-09-15 15:09:08 +0300

    Display all food details

--- FinerEli/Pages/FoodDetails.cshtml.cs
+++ FinerEli/Pages/FoodDetails.cshtml.cs
@@ -6,7 +6,6 @@ namespace FinerEli.Pages;
 
 public class FoodDetails : PageModel
 {
-    
     private readonly FoodsContext _context;
 
     public FoodDetails(FoodsContext context)
@@ -15,36 +14,50 @@ public class FoodDetails : PageModel
     }
 
     public Food Food { get; set; } = new();
-    
-    public IEnumerable<ComponentClass> ComponentClasses { get; set; } = new List<ComponentClass>();
-    
+
+    public IEnumerable<FoodDetailsGrouping> FoodDetailsData { get; set; } = new List<FoodDetailsGrouping>();
+
     public IActionResult OnGet(int id)
     {
         var food = _context.Foods.Find(id);
-        if (food == null)
-        {
-            return NotFound();
-        }
-        
+        if (food == null) return NotFound();
+
         Food = food;
 
-        var componentValues = _context.ComponentValues.Where(componentValue => componentValue.FoodId == Food.Id).ToList();
-        
-        // Get component name, value and unit, grouped by component class in LINQ syntax
-        var componentClasses = from componentValue in componentValues
+        var componentValues =
+            _context.ComponentValues.Where(componentValue => componentValue.FoodId == Food.Id).ToList();
+
+        var foodDetailsGrouping = from componentValue in componentValues
             join component in _context.Components on componentValue.EufdNameThsCode equals component.EufdNameThsCode
             join componentClass in _context.ComponentClasses on component.ComponentClassName equals componentClass.Name
             join eufdName in _context.EufdNames on componentValue.EufdNameThsCode equals eufdName.ThsCode
-            join componentUnit in _context.ComponentUnits on component.ComponentUnitName equals componentUnit.Name
-            orderby componentValue.Value descending 
-            group new {Name = eufdName.Description, componentValue.Value, Unit = componentUnit.Description} by componentClass
+            join componentUnit in _context.ComponentUnits on component.ComponentUnitName equals componentUnit
+                .EufdNameThsCode
+            orderby componentValue.Value descending
+            group new FoodDetailsModel
+                {
+                    Name = eufdName.Description, Value = componentValue.Value,
+                    Unit = componentUnit.EufdNameThsCode.ToLower()
+                } by
+                componentClass
             into g
-            select new {ComponentClass = g.Key, ComponentValues = g};
-        
-        
-        
-        ComponentClasses = componentClasses.Distinct();
+            select new FoodDetailsGrouping { ComponentClass = g.Key, FoodDetailsModels = g };
+
+        FoodDetailsData = foodDetailsGrouping;
 
         return Page();
     }
+}
+
+public class FoodDetailsGrouping
+{
+    public ComponentClass ComponentClass { get; set; } = new();
+    public IEnumerable<FoodDetailsModel> FoodDetailsModels { get; set; } = new List<FoodDetailsModel>();
+}
+
+public class FoodDetailsModel
+{
+    public string Name { get; set; } = "";
+    public double Value { get; set; }
+    public string Unit { get; set; } = "";
 }
